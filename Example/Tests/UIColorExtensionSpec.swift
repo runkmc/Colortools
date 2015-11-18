d41a64cdccc25021d3baa62b182fc879dbfe66ce
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-17 16:30:54 -0800

    Expand first test

--- Example/Tests/UIColorExtensionSpec.swift
+++ Example/Tests/UIColorExtensionSpec.swift
@@ -9,7 +9,9 @@ class UIColorExtensionsSpec: QuickSpec {
         describe("returning values") {
           let testColor = UIColor.init(hue: 0.4, saturation: 0.5, brightness: 0.6, alpha: 1.0)
           it("returns a tuple containing hue, saturation, and brightness") {
-            expect(testColor.hsb()!.hue).to(beCloseTo(0.4, within: 0.001))
+            expect(testColor.hsb()?.hue).to(beCloseTo(0.4, within: 0.001))
+            expect(testColor.hsb()?.saturation).to(beCloseTo(0.5, within: 0.001))
+            expect(testColor.hsb()?.brightness).to(beCloseTo(0.6, within: 0.001))
           }
         }
     }
