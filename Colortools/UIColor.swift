5fb4b517dd60ae139378ddfd16d45ddcb1f3a5c8
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-06-05 16:49:53 -0700

    Add instance variable hue to UIColor

--- Colortools/UIColor.swift
+++ Colortools/UIColor.swift
@@ -10,4 +10,37 @@ import UIKit
 
 extension UIColor {
 
+    public var hue: Double? {
+        return self.getHsba()?.hue
+    }
+    
+    private func getHsba() -> (hue:Double, saturation:Double, brightness:Double, alpha:Double)? {
+        let hue = UnsafeMutablePointer<CGFloat>.allocate(capacity: 1)
+        let brightness = UnsafeMutablePointer<CGFloat>.allocate(capacity: 1)
+        let saturation = UnsafeMutablePointer<CGFloat>.allocate(capacity: 1)
+        let alpha = UnsafeMutablePointer<CGFloat>.allocate(capacity: 1)
+        
+        hue.initialize(to: 0.0)
+        brightness.initialize(to: 0.0)
+        saturation.initialize(to: 0.0)
+        alpha.initialize(to: 0.0)
+        
+        var values: (hue:Double, saturation:Double, brightness:Double, alpha:Double)?
+        if self.getHue(hue, saturation: saturation, brightness: brightness, alpha: alpha) == true {
+            values = (hue: Double(hue.pointee), saturation: Double(saturation.pointee), brightness: Double(brightness.pointee), alpha: Double(alpha.pointee))
+        } else {
+            values = nil
+        }
+        hue.deinitialize(count: 1)
+        hue.deallocate(capacity: 1)
+        brightness.deinitialize(count: 1)
+        brightness.deallocate(capacity: 1)
+        saturation.deinitialize(count: 1)
+        saturation.deallocate(capacity: 1)
+        alpha.deinitialize(count: 1)
+        alpha.deallocate(capacity: 1)
+        
+        return values
+    }
+
 }
--- ColortoolsTests/ColortoolsTests.swift
+++ ColortoolsTests/ColortoolsTests.swift
@@ -21,16 +21,8 @@ class ColortoolsTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testHue() {
+        XCTAssertEqualWithAccuracy(UIColor.blue.hue!, 0.6666666, accuracy: 0.01)
     }
     
 }
