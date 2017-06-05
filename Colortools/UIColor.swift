a7c2e7df3f71aa8814d8300ee777a01b32b1beb2
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-06-05 16:43:41 -0700

    Add UIColor extension file

---
+++ Colortools/UIColor.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor.swift
+//  Colortools
+//
+//  Created by Kevin McGladdery on 6/5/17.
+//  Copyright © 2017 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+}
